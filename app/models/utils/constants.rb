57d6f31a55e50efc0483869a9a845a55b16caac9
孟庆巍 <keven@lonzh.com>
2022-05-30 10:14:16 +0800

    init

--- app/models/utils/constants.rb
+++ app/models/utils/constants.rb
@@ -5,8 +5,8 @@ module Utils
     PLAT_NAME = 'Unicorns'
     BASE_URI = 'https://www.unicorns.name/'
 
-    # CMD_DIR = Rails.root.join('web3_js')
-    CMD_DIR = '/Users/kevenmong/projects/2021-09/red_packet/src/red_packet/contracts/js/cmd'
+    CMD_DIR = Rails.root.join('web3_js')
+    # CMD_DIR = '/Users/kevenmong/projects/2021-09/red_packet/src/red_packet/contracts/js/cmd'
     BSC_PRC = 'https://bsc-dataseed1.defibit.io/'
     # BSC_PRC = 'http://localhost:8545/'
 
